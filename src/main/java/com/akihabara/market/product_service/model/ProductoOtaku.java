bc00bb13e277b305e033112b8ee5af7cc12e6c58
Livan <livanar00@gmail.com>
2025-05-21 11:27:23 +0200

    [Feature] Problema de peticion id arreglada

--- src/main/java/com/akihabara/market/product_service/model/ProductoOtaku.java
+++ src/main/java/com/akihabara/market/product_service/model/ProductoOtaku.java
@@ -1,5 +1,7 @@
 package com.akihabara.market.product_service.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -25,6 +27,8 @@ public class ProductoOtaku {
         this.stock = stock;
     }
 
+    @Schema(description = "ID generado automáticamente", accessMode = Schema.AccessMode.READ_ONLY)
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
